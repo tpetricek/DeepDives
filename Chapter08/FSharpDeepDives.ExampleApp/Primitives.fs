0d8f852b1c9edd035acb7c55e27319fe406066f2
Colin Bull <colinbul@googlemail.com>
2014-05-07 09:42:41 +0100

    Adding Chapter 8

---
+++ Chapter08/FSharpDeepDives.ExampleApp/Primitives.fs
@@ -0,0 +1,105 @@
+﻿namespace FSharpDeepDives.ExampleApp
+
+open System
+open FSharpDeepDives
+
+module Option =
+    let map2 op avg1 avg2 = 
+        match avg1, avg2 with
+        | Some(a), Some(b) -> Some (op a b)
+        | Some(a), _ -> Some a
+        | _, Some(b) -> Some b
+        | _, _ -> None
+
+[<AutoOpen>]
+module Primitives =
+
+    [<Measure>]type MW
+
+    /// <summary>
+    /// A discriminated union to represent a set of triggers
+    /// </summary>
+    type Trigger = 
+        | Cron
+        | File of string
+
+    /// <summary>
+    /// The event types available for processing
+    /// </summary>
+    type EventType =
+        | ExtractedData of Trigger * string
+        | TransformedData of Trigger * string
+        | TotalStatisticUpdated
+        | AverageStatisticUpdated
+
+    /// <summary>
+    /// A simple representation of a Settlement Period
+    /// </summary>
+    type SettlementPeriod = DateTime * int
+
+    /// <summary>
+    /// A representation of the current Power Generation Stats by fuel
+    /// </summary>
+    type FuelSettlementPeriod  = {
+        Period : SettlementPeriod
+        CCGT : int<MW>
+        OCGT : int<MW>
+        Coal : int<MW>
+        Oil : int<MW>
+        Wind : int<MW>
+        Nuclear : int<MW>
+    }
+
+    /// <summary>
+    /// A simple message representing incoming data from various sources
+    /// </summary>
+    type Message = 
+         | Data of seq<FuelSettlementPeriod>
+    
+    /// <summary>
+    /// A aggregate type that we can use to compute stastitics by 
+    /// fuel type
+    /// </summary>
+    type FuelTypeStats = {
+        CCGT : float<MW>
+        OCGT : float<MW>
+        Coal : float<MW>
+        Oil : float<MW>
+        Wind : float<MW>
+        Nuclear : float<MW>
+    }
+    with
+        static member Create(fsp : FuelSettlementPeriod) = 
+            {
+              CCGT = (float fsp.CCGT |> LanguagePrimitives.FloatWithMeasure); 
+              OCGT = (float fsp.OCGT |> LanguagePrimitives.FloatWithMeasure); 
+              Coal = (float fsp.Coal |> LanguagePrimitives.FloatWithMeasure); 
+              Oil = (float fsp.Oil  |> LanguagePrimitives.FloatWithMeasure); 
+              Wind = (float fsp.Wind  |> LanguagePrimitives.FloatWithMeasure);
+              Nuclear = (float fsp.Nuclear |> LanguagePrimitives.FloatWithMeasure)   
+            }
+        static member Zero = 
+            { CCGT = 0.<MW>; 
+              OCGT = 0.<MW>; 
+              Coal = 0.<MW>; 
+              Oil = 0.<MW>; 
+              Wind = 0.<MW>;
+              Nuclear = 0.<MW> }
+        static member DivideByInt(fsp: FuelTypeStats, count:int) = 
+            { fsp with
+                  CCGT = fsp.CCGT / (float count)
+                  OCGT = fsp.CCGT / (float count)
+                  Coal = fsp.Coal / (float count)
+                  Oil = fsp.Oil / (float count)
+                  Wind = fsp.Wind / (float count)
+                  Nuclear = fsp.Nuclear / (float count) 
+            } 
+        static member (+) (ft1, ft2) = 
+            {
+                CCGT = ft1.CCGT + ft2.CCGT
+                OCGT = ft1.OCGT + ft2.OCGT
+                Coal = ft1.Coal + ft2.Coal
+                Oil = ft1.Oil + ft2.Oil
+                Wind = ft1.Wind + ft2.Wind
+                Nuclear = ft1.Nuclear + ft2.Nuclear
+            }
---
+++ Chapter08/FSharpDeepDives.ExampleApp/Triggers.fs
@@ -0,0 +1,35 @@
+﻿namespace FSharpDeepDives.ExampleApp
+
+module Triggers = 
+    
+    open System
+    open System.IO
+    open FSharpDeepDives
+    open FSharpDeepDives.ExampleApp
+
+    let private ensurePath path = 
+        let di = new DirectoryInfo(path)
+        if di.Exists
+        then di.FullName
+        else di.Create(); di.FullName
+    
+    /// <summary>
+    /// Watches a file path for changed or created events. And extracts the target file.
+    /// </summary>
+    /// <param name="path">The file path to watch</param>
+    let file path = 
+        let watcher = new FileSystemWatcher(ensurePath path)
+        watcher.EnableRaisingEvents <- true
+        watcher.Created
+        |> Observable.filter (fun x -> (x.ChangeType = WatcherChangeTypes.Changed) 
+                                       || (x.ChangeType = WatcherChangeTypes.Created))
+        |> Observable.map (fun x -> File(x.FullPath))
+        
+    /// <summary>
+    /// Creates an observable based on a cron expression. The observable will be fired
+    /// every time the cron expression is matched.
+    /// </summary>
+    /// <param name="cronExp">The cron expression</param>
+    let cron cronExp =  
+        Schedule.toObservable cronExp
+        |> Observable.map (fun _ -> Cron)
