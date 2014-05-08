06b390da04216658a03838f99baeeabce2a744f9
Dmitry Morozov <dmitry.morozov@eikospartners.com>
2014-05-07 20:31:16 -0400

    Added Chapter 7

---
+++ Chapter07/Intro/Mvc.fs
@@ -0,0 +1,19 @@
+﻿namespace global
+
+open System
+open System.ComponentModel
+
+type IView<'TEvent, 'TModel> =
+    abstract Events : IObservable<'TEvent>
+    abstract SetBindings: model : 'TModel -> unit
+
+type IController<'TEvent, 'TModel> =
+    abstract InitModel: 'TModel -> unit
+    abstract EventHandler: 'TModel * 'TEvent -> unit
+
+module Mvc = 
+    let start (model: #INotifyPropertyChanged, view: IView<'TEvent, 'TModel>, controller: IController<_, _>) = 
+        controller.InitModel model
+        view.SetBindings model
+        view.Events.Subscribe(fun event -> controller.EventHandler(model, event))
+
